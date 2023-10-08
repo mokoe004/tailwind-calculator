079d16ea21723b198fff80f2070f43069bc8cdc7
moko002 <mohammad.koese002@stud.fh-dortmund.de>
2023-10-08 22:55:36 +0200

    Backend finished

--- src/calculator.js
+++ src/calculator.js
@@ -2,11 +2,8 @@ const display = document.getElementById("display");
 display.placeholder = "0";
 const displayTop = document.getElementById("display-top");
 
-let Operation = {
-  op1: "",
-  op2: "",
-  operator: "",
-};
+let topFree = true;
+let opFree = true;
 
 let allowedChars = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "."];
 let allowedOps = ["+", "-", "/", "*"];
@@ -16,74 +13,79 @@ let lastChar = () => display.value[display.value.length - 1];
 
 function appendToDisplay(elem) {
   display.value += elem;
-  displayTop.appendChild(document.createTextNode(elem));
-  Operation.op2 = Operation.op2 + elem;
+  if(topFree){
+    displayTop.appendChild(document.createTextNode(elem));
+  }
 }
 
 function clearDisplay() {
   display.placeholder = "0";
   display.value = "";
   displayTop.innerHTML = "";
-  Operation.op1 = "";
-  Operation.op2 = "";
-  Operation.operator = "";
+  topFree = true;
 }
 
 function operate(elem) {
   if (!isDisplayEmpty()) {
     //Append if last char is not an operator, else change the last char
-    if (allowedChars.includes(display.value[display.value.length - 1])) {
-
+    if (allowedChars.includes(lastChar()) && opFree) {
       appendToDisplay(elem);
-      Operation.operator = elem;
-      Operation.op1 = Operation.op2;
-      Operation.op2 = "";
-    } else {
-      display.value = display.value.slice(0, -1);
-      displayTop.innerHTML = displayTop.innerHTML.slice(0, -1);
-      appendToDisplay(elem);
-      Operation.operator = elem;
+      console.log("case 1");
     }
-  } else if (elem === "-") {
+    if(allowedOps.includes(lastChar())){
+      display.value = display.value.slice(0, -1);
+      if(topFree){
+        displayTop.innerHTML = displayTop.innerHTML.slice(0, -1);
+      }
+      appendToDisplay(elem);
+      console.log("case 2");
+    }
+    console.log("opFree: " + opFree);
+    opFree = false;
+  } else if ((elem === "-") && allowedChars.includes(lastChar())) {
     appendToDisplay(elem);
   } else if (elem === ".") {
     //funktioniert nicht wegen Typumwandlung in appendToDisplay
     appendToDisplay("0");
     appendToDisplay(".");
   }
-  console.log(Operation);
 }
 
 function calculate() {
   let result = 0;
-  switch (Operation.operator) {
+  let r = parseOperation(display.value);
+  switch (r.operator) {
     case "+":
-      result = parseFloat(Operation.op1) + parseFloat(Operation.op2);
+      result = parseFloat(r.operand1) + parseFloat(r.operand2);
       break;
     case "-":
-      result = parseFloat(Operation.op1) - parseFloat(Operation.op2);
+      result = parseFloat(r.operand1) - parseFloat(r.operand2);
       break;
     case "*":
-      result = parseFloat(Operation.op1) * parseFloat(Operation.op2);
+      result = parseFloat(r.operand1) * parseFloat(r.operand2);
       break;
     case "/":
-      result = parseFloat(Operation.op1) / parseFloat(Operation.op2);
+      result = parseFloat(r.operand1) / parseFloat(r.operand2);
       break;
   }
   display.value = result;
-  Operation.op2 = result;
-  Operation.op1 = "";
-  Operation.operator = "";
+  if(!topFree){
+    displayTop.innerHTML = r.operand1 + r.operator + r.operand2;
+  }else{
+    topFree = false;
+  }
+  opFree = true;
 }
 
 function erase() {
   if (!isDisplayEmpty()) {
-    if(allowedOps.includes(lastChar())){
-      Operation.operator = "";
-     }
+    if(allowedOps.includes(lastChar()) && !opFree){
+      opFree = true;
+    }
     display.value = display.value.slice(0, -1); 
-    Operation.op2 = Operation.op2.slice(0, -1);
+    if(topFree){
     displayTop.innerHTML = displayTop.innerHTML.slice(0, -1);
+    }
   }
 }
 
@@ -92,12 +94,14 @@ function switchSign() {
   if (!isDisplayEmpty()) {
     if (display.value[0] === "-") {
       display.value = display.value.slice(1);
-      Operation.op2 = Operation.op2.slice(1);
-      displayTop.innerHTML = displayTop.innerHTML.slice(1);
+      if(topFree){
+        displayTop.innerHTML = displayTop.innerHTML.slice(1);
+      }
     } else {
       display.value = "-" + display.value;
-      Operation.op2 = "-" + Operation.op2;
-      displayTop.innerHTML = "-" + displayTop.innerHTML;
+      if(topFree){
+        displayTop.innerHTML = "-" + displayTop.innerHTML;
+      }
     }
   }
 }
@@ -105,7 +109,6 @@ function switchSign() {
 //Keyboard funktion
 document.onkeydown = function (event) {
   let key = event.key;
-  console.log(key);
   if (allowedChars.includes(key)) {
     appendToDisplay(key);
   }
@@ -122,3 +125,29 @@ document.onkeydown = function (event) {
     clearDisplay();
   }
 };
+
+function parseOperation(str) {
+  let operator = "";
+  let operand1 = "";
+  let minus = false;
+  let operand2 = "";
+
+  if(str[0] === "-"){
+    str = str.slice(1,str.length);
+    minus = true;
+  }
+  for (let i = 0; i < str.length; i++) {
+    if (allowedOps.includes(str[i])) {
+      operator = str[i];
+      operand1 = str.slice(0, i);
+      operand2 = str.slice(i + 1);
+      break;
+    }
+  }
+
+  if(minus){
+    operand1 = "-" + operand1;
+  }
+  return { operator, operand1, operand2 };
+}
+
